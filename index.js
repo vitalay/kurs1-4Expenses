8d08375e08cc75938420d7817c380a6479f30dc7
vitalya.efimov <vitalay.efimov@gmail.com>
2024-11-22 21:31:19 +0500

    9

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
+const LIMIT = 10000;
+
 const expenses = [];
 
+const inputNode = document.querySelector(".js-exspense-input");
+const btnNode = document.querySelector(".js-btn");
+const historyNode = document.querySelector(".js-history");
+const sumNode = document.querySelector(".js-sum");
+const limitNode = document.querySelector(".js-limit");  
 
-const inputNode = document.querySelector('.js-exspense-input');
-const btnNode = document.querySelector('.js-btn');
-const historyNode = document.querySelector('.js-history');
+limitNode.innerText = LIMIT;
 
-btnNode.addEventListener('click', () => {
-    //получаем значение из инпута
-    if (inputNode.value === '') {
-       return;
-    }
-  
-const expense = parseInt(inputNode.value);
+btnNode.addEventListener("click", () => {
+  //получаем значение из инпута
+  if (inputNode.value === "") {
+    return;
+  }
 
-expenses.push(expense);
+  const expense = parseInt(inputNode.value);
+
+  expenses.push(expense);
+
+  inputNode.value = "";
+
+  //Сохраняем трату в список
+
+  //выводим новый список трат
+  let expensesListHTML = "";
+
+  expenses.forEach((element) => {
+    expensesListHTML += `<li>${element} руб.</li> `;
+  });
+
+  historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+
+  // Получаем общую сумму трат
+
+  let sum = 0;
+
+  expenses.forEach((element) => {
+    sum += element;
+  });
+
+  sumNode.innerText = sum;
 
 
-inputNode.value = '';
-
-//Сохраняем трату в список
-
-
-
-//выводим новый список трат
-let expensesListHTML ='';
-
-
-expenses.forEach(element => {
-   
-    expensesListHTML += `<li>${element}</li>`;
-  
 });
-
-
-
-historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
-});
-
-
